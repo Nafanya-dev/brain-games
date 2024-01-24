e44d0a35917cd12062d3666cf52bab154337e3c4
Gaevskii.V <gaevskii.v95@gmail.com>
2024-01-24 20:25:08 +0200

    step 3
    add brain_games/cli.py
    fix brain_games/skripts/brain_games.py

---
+++ brain_games/cli.py
@@ -0,0 +1,5 @@
+import prompt
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+from brain_games.cli import welcome_user
+
 def greeting():
     print('Welcome to the Brain Games!')
 
 
 def main():
     greeting()
+    welcome_user()
 
 
 if __name__ == '__main__':
